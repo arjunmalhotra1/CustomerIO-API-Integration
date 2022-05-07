500f44a087b12e1c1ea493634d34502dccff95c3
Arjun Malhotra <am2cj@virginia.edu>
2022-05-07 02:21:04 -0500

    Working - Sending data with customer.io library.

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
+	"sync"
+
+	"github.com/customerio/go-customerio"
 )
 
 type Mapping struct {
@@ -40,25 +41,11 @@ func readConfigFile(fileString string) {
 	if err != nil {
 		log.Fatal("Error while un-marshalling the data", err)
 	}
-	//fmt.Println("%+v", configData)
 	fmt.Println("Read Config file completed")
 }
 
-// func readDataFile(fileString string) []byte {
-// 	content, err := ioutil.ReadFile("data.json")
-// 	if err != nil {
-// 		log.Fatal("Error while opening the file - ", err)
-// 	}
-
-// 	err = json.Unmarshal(content, &data)
-// 	if err != nil {
-// 		log.Fatal("Error while un-marshalling the data", err)
-// 	}
-// 	return content
-// }
-
 func readDataFile(filePath string) {
-	readChan = make(chan dataSend)
+
 	file, err := os.Open(filePath)
 	if err != nil {
 		log.Fatalln("Error while opening the file: ", err)
@@ -67,94 +54,84 @@ func readDataFile(filePath string) {
 
 	decoder := json.NewDecoder(file)
 
-	tken, err := decoder.Token()
+	_, err = decoder.Token()
 	if err != nil {
-		log.Fatalln("Error while decoding a token: ", err)
+		log.Fatalln("Error while reading the opening token: ", err)
 	}
-	fmt.Println(tken)
+	//fmt.Println(tken)
 
-	count := 0
+	//count := 0
 
 	for decoder.More() {
-		fmt.Println(count)
-		count++
+		//fmt.Println(count)
+		//count++
 		temp := make(map[string]interface{})
-		var id string
+		var Cid string
 		decoder.Decode(&data)
 		for _, k := range configData.Mappings {
 			if k.From == configData.UserId {
-				id = fmt.Sprint(data[k.From])
-				log.Println(id)
+				Cid = fmt.Sprint(data[k.From])
+				log.Println(Cid)
 			}
 			temp[k.To] = data[k.From]
 		}
 		ds := dataSend{
 			Body: temp,
-			id:   id,
+			id:   Cid,
 		}
-		//fmt.Println(ds)
 		readChan <- ds
+	}
+	_, err = decoder.Token()
+	if err != nil {
+		log.Fatalln("Error while reading the ending token: ", err)
 	}
 	fmt.Println("Read Data file completed")
 	//close(readChan)
 }
 
 func main() {
-	// var wg sync.WaitGroup
+	readChan = make(chan dataSend)
+	var wg sync.WaitGroup
 	readConfigFile("configuration.json")
 	children := configData.Parallelism
 	fmt.Println("children", children)
+	track := customerio.NewTrackClient("e7192b0752ef138df135", "89f446c3ada96eb5c73b", customerio.WithRegion(customerio.RegionUS))
 	for c := 0; c < children; c++ {
-		// 	wg.Add(1)
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			for ds := range readChan {
+				//time.Sleep(1000)
 				//fmt.Println(ds)
-				//defer wg.Done()
-				// initialize http client
-				client := &http.Client{}
-				jsonBody, _ := json.Marshal(ds.Body)
-				// set the HTTP method, url, and request body
-				req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("https://track.customer.io/api/v1/customers/%s", ds.id), bytes.NewBuffer(jsonBody))
-				if err != nil {
-					panic(err)
+				if err := track.Identify(ds.id, ds.Body); err != nil {
+					log.Println(err)
 				}
-				var bearer = "Bearer " + "e7192b0752ef138df135:89f446c3ada96eb5c73b"
-				// add authorization header to the req
-				req.Header.Add("Authorization", bearer)
-				// set the request header Content-Type for json
-				req.Header.Set("Content-Type", "application/json; charset=utf-8")
-				resp, err := client.Do(req)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(resp.StatusCode)
+				// // initialize http client
+				// client := &http.Client{}
+				// jsonBody, _ := json.Marshal(ds.Body)
+				// // set the HTTP method, url, and request body
+				// req, err := http.NewRequest(http.MethodPut, fmt.Sprintf("https://track.customer.io/api/v1/customers/%s", ds.id), bytes.NewBuffer(jsonBody))
+				// if err != nil {
+				// 	panic(err)
+				// }
+				// var bearer = "Bearer " + "e7192b0752ef138df135:89f446c3ada96eb5c73b"
+				// // add authorization header to the req
+				// req.Header.Set("Authorization", bearer)
+				// // set the request header Content-Type for json
+				// req.Header.Set("Content-Type", "application/json; charset=utf-8")
+				// resp, err := client.Do(req)
+				// if err != nil {
+				// 	panic(err)
+				// }
+				// fmt.Println("Here")
+				// fmt.Println("resp:", resp.StatusCode)
 			}
+			fmt.Println("Shut down signal received")
 		}()
 	}
 
 	readDataFile("data.json")
-	//close(readChan)
-	//	wg.Wait()
-
-	//fmt.Println(data[0]["bio"])
-	//fmt.Println(len(data))
-
-	// track := customerio.NewTrackClient("e7192b0752ef138df135", "89f446c3ada96eb5c73b", customerio.WithRegion(customerio.RegionUS))
-
-	// for _, v := range data {
-	// 	temp := make(map[string]interface{})
-	// 	var id string
-	// 	for _, k := range configData.Mappings {
-	// 		if k.From == configData.UserId {
-	// 			id = fmt.Sprint(v[k.From])
-	// 			fmt.Println(id)
-	// 		}
-	// 		temp[k.To] = v[k.From]
-	// 	}
-
-	// 	if err := track.Identify(id, temp); err != nil {
-	// 		log.Println(err)
-	// 	}
-	// }
-
+	close(readChan)
+	wg.Wait()
+	fmt.Println("Waiting")
 }
